899976cc95c946ebec9c1f7b27a743eedc105a8a
silamed123 <silamed123@gmail.com>
2026-02-28 14:00:31 +0000

    cc

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -34,8 +34,10 @@ void kmain(uint32_t magic, uint32_t multiboot_info) {
     delay(0x10000000);
     cleanscreen(' ', 0x0f);
     changcursor_color(0x0f);
+    setdefault_color(0x0f);
+    struct FAT32* fat;
     char* file = (char*)0x200000;
-    if (fat32_init() == 0) {
+    if (fat32_init(fat) == 0) {
         if (fat32_read_file(fat, "/hello.txt", (uint8_t*)file, 511) > 0) {
             file[511] = '\0';
             print_string(file);
