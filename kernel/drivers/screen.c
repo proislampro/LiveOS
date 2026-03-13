22a04d3f0abced992c5185770d2ccddb1690e0bd
silamed123 <silamed123@gmail.com>
2026-03-13 18:04:55 +0000

    cc

--- kernel/drivers/screen.c
+++ kernel/drivers/screen.c
@@ -4,50 +4,36 @@ struct multiboot_tag_framebuffer {
     uint32_t type;
     uint32_t size;
 
-    uint64_t framebuffer_addr;
-    uint32_t framebuffer_pitch;
-    uint32_t framebuffer_width;
-    uint32_t framebuffer_height;
-    uint8_t framebuffer_bpp;
-    uint8_t framebuffer_type;
+    uint64_t addr;
+    uint32_t pitch;
+    uint32_t width;
+    uint32_t height;
+    uint8_t bpp;
+    uint8_t type;
     uint16_t reserved;
 };
 
-uint32_t* framebuffer;
-uint32_t width;
-uint32_t height;
-uint32_t pitch;
 
+void init_screen(uint64_t multiboot_info) {
+    uint8_t* tag = (uint8_t*)multiboot_info + 8;
 
-void init_screen(uint64_t multiboot_info)
-{
-    uint8_t *tag = (uint8_t *)multiboot_info + 8;
-
-    while (1)
-    {
+    while (1) {
         struct multiboot_tag *t = (struct multiboot_tag*)tag;
-
-        if (t->type == 8) // framebuffer tag
-        {
+        if (t->type == 8) {
             struct multiboot_tag_framebuffer *fb = (struct multiboot_tag_framebuffer*)tag;
-
             return;
         }
-
-        if (t->type == 0) // end tag
-            break;
-
+        if (t->type == 0) break;
         tag += (t->size + 7) & ~7;
-    }
 
-    while (1) {} // framebuffer not found
+    }
+    while (1) {}
 }
 
-void putpixel(int x, int y, uint32_t color)
-{
+void putpixel(int x, int y, uint32_t color) {
     uint32_t* pixel;
 
-    pixel = (uint32_t*)((uint8_t*)framebuffer + y * pitch + x * 4);
+    pixel = (uint32_t*)((uint8_t*)f-> + y * pitch + x * 4);
 
     *pixel = color;
 }
