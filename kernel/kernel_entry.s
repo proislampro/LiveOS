632c9dfde2e6297387f51794b0c98c89525ddd0e
proislampro <137497399+proislampro@users.noreply.github.com>
2026-04-19 17:39:05 +0100

    Remove Limine request section from kernel entry

--- kernel/kernel_entry.s
+++ kernel/kernel_entry.s
@@ -1,20 +1,3 @@
-section .limine_requests
-align 8
-global limine_requests_start
-limine_requests_start:
-    dq 0
-
-global limine_base_revision
-limine_base_revision:
-    dq 0xf9562b2d5c95a6c8
-    dq 0x6a7b384944536bdc
-    dq 3
-
-align 8
-global limine_requests_end
-limine_requests_end:
-    dq 0
-
 section .text
 global _start
 extern kmain
