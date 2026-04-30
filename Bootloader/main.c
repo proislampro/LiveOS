6ef0b4fca5b8e238406b2bb1bd492f12cc711c84
proislampro <silamed123@gmail.com>
2026-04-30 20:21:09 +0100

    cc

--- Bootloader/main.c
+++ Bootloader/main.c
@@ -3,6 +3,11 @@
 
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello world\n");
-  for(;;) asm volatile("hlt");
+    InitializeLib(ImageHandle, SystemTable);
+
+    Print(L"Hello, World!\n");
+
+    while (1) {};
+
+    return EFI_SUCCESS;
 }
