7411a80c83b1801eff42d28386eb637a70f5af04
proislampro <silamed123@gmail.com>
2026-04-30 18:33:31 +0100

    cc

--- Bootloader/main.c
+++ Bootloader/main.c
@@ -1,15 +1,13 @@
 #include <efi.h>
 #include <efilib.h>
 
-// Standard UEFI entry point
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    // Initialize the UEFI library
     InitializeLib(ImageHandle, SystemTable);
 
-    // Print a simple message to the screen
-    Print(L"Hello, UEFI Bootloader!\n");
+    Print(L"Hello, World!\n");
 
-    // Keep the message on screen (waits for a keystroke)
+    
     SystemTable->ConIn->Reset(SystemTable->ConIn, FALSE);
     EFI_INPUT_KEY Key;
     while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) != EFI_SUCCESS);
