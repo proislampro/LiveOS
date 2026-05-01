0d3c57980f3bcba3466407f270a0efb6efddc97c
proislampro <silamed123@gmail.com>
2026-05-01 12:48:51 +0100

    cc

--- Bootloader/main.c
+++ Bootloader/main.c
@@ -5,7 +5,7 @@
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     InitializeLib(ImageHandle, SystemTable);
 
-    Print(L"Hello, World!\n");
+    // Print(L"Hello, World!\n");
     
     EFI_INPUT_KEY Key;
     while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) != EFI_SUCCESS);
