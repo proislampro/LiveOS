84f7864db959ecf7f464886e4ee54a7aec3fea24
proislampro <silamed123@gmail.com>
2026-05-01 14:47:21 +0100

    cc

--- Bootloader/main.c
+++ Bootloader/main.c
@@ -1,11 +1,9 @@
 #include <uefi.h>
 
 int main(int argc, char **argv) {
-    // Standard printf works for UEFI console output
     printf("Hello, World!\n");
 
-    // Wait for a single keystroke
-    getchar();
+    while (1);
 
     return 0;
 }
